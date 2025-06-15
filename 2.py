18eab13d2737e0b6f0cce20f9fd74268c7117c86
Dilip Mansukhani <contactdilip2001@gmail.com>
2025-06-15 22:16:46 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,23 @@
+# Write the even elements from the list
+mylist= [10,201,401,210,3,4,5,8]
+
+size = len(mylist)
+for i in range(0,size):
+    if(mylist[i]%2==0):
+        print(mylist[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
