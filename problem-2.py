48b67468a01b7fda2b184714b3b980ec59a32391
Dilip Mansukhani <contactdilip2001@gmail.com>
2025-07-11 20:39:33 +0530

    Add files via upload

---
+++ problem-2.py
@@ -0,0 +1,11 @@
+'''
+Problem 2: Write a program that take a user input of three angles and will find out whether it can form a triangle or not.'''
+
+a= int(input("enter the vslue of 1st angle" ))
+b= int(input("enter the vslue of 1st angle"))
+c= int(input("enter the vslue of 1st angle" ))
+
+if a + b + c == 180 :
+  print ("tringle is valid ")
+else:
+  print("triangle is invalid ")
