8830bf282d52a8d9d8cda27dc48cffda002b2d83
Dilip Mansukhani <contactdilip2001@gmail.com>
2025-07-14 19:47:25 +0530

    Add files via upload

--- problem-1.py
+++ problem-1.py
@@ -1,44 +1,9 @@
-'''
-Problem 1: Write a program that will give you in hand monthly salary after deduction on CTC - HRA(10%), DA(5%), PF(3%) and taxes deduction as below:
-Salary(Lakhs) : Tax(%)
+# Find the length of a given string without using the len() function
 
-Below 5 : 0%
-5-10 : 10%
-10-20 : 20%
-aboove 20 : 30%'''
+s = input("enter any string")
 
+count = 0
+for i in s:
+    count+=1 
+print("total number of a string:",count)
 
-# Input CTC in lakhs
-ctc_lakhs = float(input("Enter your CTC (in lakhs): "))
-
-# Convert to rupees
-salary = ctc_lakhs * 100000
-
-# Deductions
-hra = salary * 0.10
-da = salary * 0.05
-pf = salary * 0.03
-total_deductions = hra + da + pf
-
-# In-hand salary before tax
-in_hand_salary = salary - total_deductions
-
-# Tax calculation
-if ctc_lakhs < 5:
-    tax_rate = 0
-elif 5 <= ctc_lakhs < 10:
-    tax_rate = 0.10
-elif 10 <= ctc_lakhs <= 20:
-    tax_rate = 0.20
-else:
-    tax_rate = 0.30
-
-# Apply tax
-tax_amount = in_hand_salary * tax_rate
-net_salary = in_hand_salary - tax_amount
-
-# Monthly salary
-monthly_salary = net_salary / 12
-
-# Output
-print(monthly_salary)
