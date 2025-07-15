bbd9f6052a725573c68c2e5ff272c9b664c3eb84
Dilip Mansukhani <contactdilip2001@gmail.com>
2025-07-15 22:47:08 +0530

    Add files via upload

--- problem-2.py
+++ problem-2.py
@@ -1,11 +1,8 @@
-'''
-Problem 2: Write a program that take a user input of three angles and will find out whether it can form a triangle or not.'''
+# Extract username from a given email. 
+# Eg if the email is nitish24singh@gmail.com 
+# then the username should be nitish24singh
 
-a= int(input("enter the vslue of 1st angle" ))
-b= int(input("enter the vslue of 1st angle"))
-c= int(input("enter the vslue of 1st angle" ))
 
-if a + b + c == 180 :
-  print ("tringle is valid ")
-else:
-  print("triangle is invalid ")
+s = input("enter the email")
+position = s.index('@') 
+print(s[0:position])
