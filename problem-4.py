45090b0c9301dc563631af06c74364312e04d974
Dilip Mansukhani <contactdilip2001@gmail.com>
2025-07-19 23:18:49 +0530

    Add files via upload

--- problem-4.py
+++ problem-4.py
@@ -1,35 +1,12 @@
-'''
-`Problem 4`: Write a menu-driven program - 
-1. cm to ft 
-2. km to miles 
-3. USD to INR 
-4. exit
-'''
+# Write a program which can remove a particular character from a string.
 
-a = int(input("""Menu:
-1. cm to ft 
-2. km to miles 
-3. USD to INR 
-4. Exit
-Enter your choice (1-4): """))
+s = input('enter any string')
+term = input('anything you want to remove?')
 
-if a == 1:
-    x = float(input("Enter the number in cm: "))
-    ft = x * 0.0328084
-    print(f"{x} cm = {ft:.2f} feet")
+result = ""
 
-elif a == 2:
-    x = float(input("Enter the number in km: "))
-    miles = x * 0.621371
-    print(f"{x} km = {miles:.2f} miles")
-
-elif a == 3:
-    x = float(input("Enter the amount in USD: "))
-    inr = x * 83.0  # Update the rate as needed
-    print(f"${x} = ₹{inr:.2f}")
-
-elif a == 4:
-    print("Exiting the program. Thank you!")
-
-else:
-    print("Invalid choice! Please select a valid option (1 to 4).")
+for i in s:
+    if i != term:  # not in isliye use kiya jo term m likhege vo hatana h(usko chor kr ajayega baki ka)
+                        # == krte toh jo hatana h wahi ajata 
+        result+=i      #result = result + i 
+print("modified string:", result)
