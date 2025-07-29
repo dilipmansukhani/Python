73138791c9e3312fb20a8d3c9ae48466488ae022
Dilip Mansukhani <contactdilip2001@gmail.com>
2025-07-29 22:39:32 +0530

    Add files via upload

---
+++ pr18.py
@@ -0,0 +1,15 @@
+for i in range(1,5):
+    for s in range(i,4):
+        print(" ",end="")
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+for i in range(1,4):
+    for s in range(1,i+1):
+        print(" ",end="")
+    for j in range(i,4):
+        print("*",end="")
+    print()
+    
+
+    
