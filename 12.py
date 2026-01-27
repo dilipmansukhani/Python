06946667dffe7fa0ba2cf612bdaa501df0a031d0
Dilip Mansukhani <contactdilip2001@gmail.com>
2026-01-27 16:34:42 +0530

    Delete 12.py

--- 12.py
+++
@@ -1,9 +0,0 @@
-# write a programne that add two elements and give target value.
-
-list = [6, 3, 4, 5,1 ,8]
-target = 9
-
-for i in range(0,len(list)):
-    for j in range(1+i,len(list)):
-        if list[i] + list[j] == target:
-            print(list[i],list[j])
