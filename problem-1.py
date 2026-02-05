5bc4bf5368f5947b136df9fb6cf35d576ca04c0c
Dilip Mansukhani <contactdilip2001@gmail.com>
2026-02-05 16:57:12 +0530

    Delete problem-1.py

--- problem-1.py
+++
@@ -1,9 +0,0 @@
-# Find the length of a given string without using the len() function
-
-s = input("enter any string")
-
-count = 0
-for i in s:
-    count+=1 
-print("total number of a string:",count)
-
